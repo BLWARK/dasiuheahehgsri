b1fade06b477c69f92373eed3a7e9a00ff2fab60
Ronald <rozu0980@gmail.com>
2025-02-05 14:22:51 +0700

    fixing error

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Hero from "@/components/page-components/hero/Hero";
 import About from "@/components/page-components/about/About";
 import Keunggulan from "@/components/page-components/keunggulan/Keunggulan";
